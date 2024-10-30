ccbf87604a6d7aa900964d3aeeed1b2988f27df1
Salomé <salome.cliquennois@viacesi.fr>
2024-10-30 10:56:19 +0100

    WIP

--- Application/User/Commands/AddUserCommand.cs
+++ Application/User/Commands/AddUserCommand.cs
@@ -2,12 +2,37 @@
 using Choix_des_technos_et_infras_de_développement___TP1.Domain;
 using Choix_des_technos_et_infras_de_développement___TP1.Persistence;
 using Microsoft.EntityFrameworkCore;
+using MQTTnet;
+using MQTTnet.Client;
 
 namespace Choix_des_technos_et_infras_de_développement___TP1.Application.User.Commands
 {
     public class AddUserCommand : UseCaseBase
     {
-        public AddUserCommand(TP1Context dbContext) : base(dbContext) { }
+        private IMqttClient _mqttClient { get; set; }
+
+        public AddUserCommand(TP1Context dbContext) : base(dbContext) {
+            var broker = "http://localhost";
+            var port = 1883;
+            var clientId = Guid.NewGuid().ToString();
+
+            // Create a MQTT client factory
+            var factory = new MqttFactory();
+
+            // Create a MQTT client instance
+            var mqttClient = factory.CreateMqttClient();
+
+            // Create MQTT client options
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(broker, port) // MQTT broker address and port
+                .WithClientId(clientId)
+                .WithCleanSession()
+                .Build();
+            
+            mqttClient.ConnectAsync(options);
+
+            _mqttClient = mqttClient;
+        }
 
         public async Task AddUserAsync(UserModel user, CancellationToken cancellationToken)
         {
@@ -33,6 +58,14 @@ namespace Choix_des_technos_et_infras_de_développement___TP1.Application.User.C
 
                 await _dbContext.Users.AddAsync(userToAdd, cancellationToken);
                 await _dbContext.SaveChangesAsync(cancellationToken);
+
+                var mqttMessage = new MqttApplicationMessage
+                {
+                    PayloadSegment = new ArraySegment<byte>(BitConverter.GetBytes(userToAdd.Id)),
+                    Topic = "Event/NewUser"
+                };
+
+                await _mqttClient.PublishAsync(mqttMessage, cancellationToken);
             }
             catch (Exception)
             {
